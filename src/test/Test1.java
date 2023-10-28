6d436616df7e20eab1f0fe52172958950b6937c8
PriyaPRaut <priya.raut788@gmail.com>
2023-10-28 07:22:22 +0530

    First Commit....

---
+++ src/test/Test1.java
@@ -0,0 +1,20 @@
+package test;
+
+public class Test1 {
+	public static void main(String[] args) {
+		
+		String String = "hungry";
+		
+		java.lang.String hungry = null;
+		if(hungry=="yes")
+		{
+			System.out.println("Eat mething");
+		}
+		
+		else
+		{
+			System.out.println("Do Work");
+		}
+	}
+
+}
