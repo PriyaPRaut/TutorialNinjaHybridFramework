4081d6a942bee0ec1338c7f195ba35580938a870
PriyaPRaut <priya.raut788@gmail.com>
2024-02-29 23:06:35 +0530

    Added comment

---
+++ src/Test1/Test.java
@@ -0,0 +1,21 @@
+package Test1;
+
+//Add comment
+public class Test {
+public static void main(String[] args) {
+		
+		String String = "hungry";
+		
+		java.lang.String hungry = null;
+		if(hungry=="yes")
+		{
+			System.out.println("Eat mething");
+		}
+		
+		else
+		{
+			System.out.println("Do Work");
+		}
+	}
+
+}
